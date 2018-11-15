51f39a33ab844215641594a00d54cabfab5ef06b
sung <s.kim.castru@gmail.com>
2018-11-15 20:17:42 +0200

    Setting Weather object and getWeather metod

--- src/app/Weather.js
+++ src/app/Weather.js
@@ -1,7 +1,15 @@
-export class Weater {
+export class Weather {
   constructor(city, countryCode) {
     this.apiKey = "328d874cf805b68f6271e8f6454a53b3";
     this.city = city;
     this.countryCode = countryCode;
+  }
+
+  async getWeather() {
+    const URI = `http://api.openweathermap.org/data/2.5/weather?q=${
+      this.city
+    },${this.countryCode}&appid=${this.apiKey}`;
+    const data = await fetch(URI);
+    return data;
   }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,10 @@
+const { Weather } = require("./Weather");
+const weather = new Weather("Vilnius", "lt");
 require("./index.css");
+
+async function fetchWeather() {
+  const data = await weather.getWeather();
+  console.log(data);
+}
+
+document.addEventListener("DOMContentLoaded", fetchWeather);
