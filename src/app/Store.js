2bbc4bd3a6486444d00937790725eb3e15aa3a82
sung <s.kim.castru@gmail.com>
2018-11-18 22:06:34 +0200

    Compleating Web local storage

---
+++ src/app/Store.js
@@ -0,0 +1,32 @@
+// almacenar en local storage
+
+export class Store {
+  constructor() {
+    this.city;
+    this.countryCode;
+    this.defaultCity = "Vilnius";
+    this.defaultCountry = "lt";
+  }
+
+  getLocationData() {
+    if (localStorage.getItem("city") === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem("city");
+    }
+
+    localStorage.getItem("countryCode") === null
+      ? (this.countryCode = this.defaultCountry)
+      : (this.countryCode = localStorage.getItem("countryCode"));
+
+    return {
+      city: this.city,
+      countryCode: this.countryCode
+    };
+  }
+
+  setLocationData(city, countryCode) {
+    localStorage.setItem("city", city);
+    localStorage.setItem("countryCode", countryCode);
+  }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,11 @@
 const { Weather } = require("./Weather");
 const { UI } = require("./Ui");
+const { Store } = require("./Store");
 
-const weather = new Weather("Vilnius", "lt");
+const store = new Store();
+const { city, countryCode } = store.getLocationData();
+
+const weather = new Weather(city, countryCode);
 const ui = new UI();
 
 require("./index.css");
@@ -18,7 +22,7 @@ document.getElementById("w-change-btn").addEventListener("click", e => {
   const countryCode = document.getElementById("countryCode").value;
   weather.changeLocation(city, countryCode);
   fetchWeather();
-  console.log(city, countryCode);
+  store.setLocationData(city, countryCode);
 });
 
 document.addEventListener("DOMContentLoaded", fetchWeather);
