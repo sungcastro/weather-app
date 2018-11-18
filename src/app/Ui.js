94c00ccd957b34008eac4dfedeadd4bbf896bb39
sung <s.kim.castru@gmail.com>
2018-11-18 21:00:55 +0200

    Adding day/ month to the UI

--- src/app/Ui.js
+++ src/app/Ui.js
@@ -1,6 +1,7 @@
 export class UI {
   constructor() {
     this.location = document.getElementById("weather-location");
+    this.day = document.getElementById("weather-day");
     this.description = document.getElementById("weather-description");
     this.string = document.getElementById("weather-string");
     this.humidity = document.getElementById("weather-humidity");
@@ -9,6 +10,10 @@ export class UI {
 
   render(weather) {
     this.location.textContent = weather.name + "/" + weather.sys.country;
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    this.day.textContent = `${day} / ${month}`;
     this.description.textContent = weather.weather[0]["description"];
     this.string.textContent = weather.main.temp + `°C`;
     this.humidity.textContent = `Humidity ` + weather.main.humidity;
--- src/app/Weather.js
+++ src/app/Weather.js
@@ -8,7 +8,7 @@ export class Weather {
   async getWeather() {
     const URI = `http://api.openweathermap.org/data/2.5/weather?q=${
       this.city
-    },${this.countryCode}&appid=${this.apiKey}&units=metric&lang={lt}`;
+    },${this.countryCode}&appid=${this.apiKey}&units=metric`;
     const response = await fetch(URI);
     const data = await response.json();
     return data;
